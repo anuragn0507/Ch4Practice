ad67f3f629c3e3b2c0042a16d4ae73739da7923b
Anurag <anuragn1107@gmail.com>
2021-09-30 01:50:02 +0530

    Final Quiz app

--- app/src/main/java/com/anurag/ch4practice/MainActivity.java
+++ app/src/main/java/com/anurag/ch4practice/MainActivity.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String [] questions = {"Who has developed by who ?", "Java is introduced in 1755",
-            "Java was written c ", "Is Java was Written in Python"};
-    private boolean [] answers = {false, false, true, false};
+    private String [] questions = {"Static functions can be accessed using null reference ?", "Can we have multiple classes in same java file?",
+            "is 0.0d the default value of double variable? ", "Is String is primary data type"};
+    private boolean [] answers = {false, true , true, false};
     private int score = 0;
     Button yes;
     Button no;
@@ -24,27 +25,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         yes = findViewById(R.id.yes);
         no = findViewById(R.id.no);
-
         question =findViewById(R.id.question);
+        question.setText(questions[index]);
         yes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(answers[index]){
-                    score++;
+                //If the array is not going out of bounds
+                if (index <= questions.length - 1) {
+                    //If you given correct answer
+                    if (answers[index] == true) {
+                        score++;
+                    }
+                    index++;
+                    if (index <= questions.length - 1) {
+                        question.setText(questions[index]);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Your score is " + score + "/" +  questions.length , Toast.LENGTH_SHORT).show();
+                    }
                 }
-                question.setText(questions[index++]);
+                else{
+                    Toast.makeText(MainActivity.this, "Restart the app to play again", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
         no.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(answers[index] == false) {
-                    score++;
-
+                //If the array is not going out of bounds
+                if (index <= questions.length - 1) {
+                    //If you given correct answer
+                    if (answers[index] == false) {
+                        score++;
+                    }
+                    index++;
+                    if (index <= questions.length - 1) {
+                        question.setText(questions[index]);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Your score is " + score + "/" +  questions.length, Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, "Restart the app to play again", Toast.LENGTH_SHORT).show();
                 }
-                question.setText(questions[index++]);
+
 
             }
+
         });
 
     }
